480b46d5c499d0775552e8d801f6a3eed18fb9fb
Jobson Batista <51756630+jobson-batista@users.noreply.github.com>
2024-01-10 11:46:05 -0300

    Update CREATE_DATABASE_AND_TABLE.sql

--- scripts/CREATE_DATABASE_AND_TABLE.sql
+++ scripts/CREATE_DATABASE_AND_TABLE.sql
@@ -3,7 +3,7 @@ create database if not exists prova_remsoft;
 use prova_remsoft;
 
 create table tarefa (
-	id int(11) auto_increment primary key,
+    id int(11) auto_increment primary key,
     descricao varchar(255) not null,
     finalizado tinyint(1) default 0,
     dt_finalizado datetime,
@@ -12,7 +12,3 @@ create table tarefa (
     excluido tinyint(1) default 0
 );
 commit;
-
-select * from tarefa;
--- update tarefa set excluido = 1 where id = 0;
--- update tarefa set excluido = 1 where id = 2;
